c148cf8b9ab0a38d02d8ab1e9ee52736b34cae7b
Kamilunavo <acciento89@gmail.com>
2026-08-21 22:49:08 +0200

    android: add lifecycle compose support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.12.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
 
     implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.compose.ui:ui")
